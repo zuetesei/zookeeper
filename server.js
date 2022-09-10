8260e303a18033e7152d02b17392438cd0b50f3d
Zuleika Tesei <zuleikatesei@zuleikas-mbp.lan>
2022-09-09 17:56:59 -0700

    new name plus port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { animals } = require('./data/animals.json');
+
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 app.get('/api/animals', (req, res) => {
