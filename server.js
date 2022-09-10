4938a674c093c7f9db70eafdafcb72e7277b030f
Zuleika Tesei <zuleikatesei@zuleikas-mbp.lan>
2022-09-09 19:47:42 -0700

    set up express.js server and GET routes

--- server.js
+++ server.js
@@ -5,12 +5,24 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
